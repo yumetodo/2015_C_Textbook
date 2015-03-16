7fc67509cdf8b5299b738eb831ee4996a88953ee
yumetodo <yume-wikijp@live.jp>
2015-03-16 18:47:18 +0900

    cssの色修正及び.cの文字コードをUTF-8withBOMに

--- src/002_show_argv.c
+++ src/002_show_argv.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+ï»¿#include <stdio.h>
 int main(int argc, char* argv[]){
 	if (argc < 1) return -1;
 	unsigned int i;
-	for (i = 0; i < argc; i++){/* argc‰ñ•\Ž¦‚·‚é */
-		printf("argv[%d]:%s\n", i, argv[i]);//•\Ž¦•”•ª
+	for (i = 0; i < argc; i++){/* argcå›žè¡¨ç¤ºã™ã‚‹ */
+		printf("argv[%d]:%s\n", i, argv[i]);//è¡¨ç¤ºéƒ¨åˆ†
 	}
 	return 0;
 }
