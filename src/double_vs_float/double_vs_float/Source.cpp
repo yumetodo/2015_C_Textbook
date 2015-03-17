795ff4b2bbecce61616cdb913bc75f11dbd3b6d9
yumetodo <yume-wikijp@live.jp>
2015-03-17 23:31:47 +0900

    変数とはの解説追加。

---
+++ src/double_vs_float/double_vs_float/Source.cpp
@@ -0,0 +1,46 @@
+﻿#include <windows.h>
+#include <stdio.h>
+#include <time.h>
+#ifndef __GNUC__
+#pragma comment(lib, "setupapi.lib")
+#pragma comment(lib, "hid.lib")
+#pragma comment(lib, "winmm.lib")
+#endif
+#define TEST_TIME 1000000000
+void floatloop(void)
+{
+	int     i;
+	float   r = 0.0;
+
+	for (i = 0; i < TEST_TIME; ++i)
+		r += (float)0.1;
+	printf("r=%g\n", r);
+}
+
+void doubleloop(void)
+{
+	int     i;
+	double  r = 0.0;
+
+	for (i = 0; i < TEST_TIME; ++i)
+		r += 0.1;
+	printf("r=%g\n", r);
+}
+int main(void)
+{
+	clock_t startTimef, startTimed, endTimef, endTimed;
+	timeBeginPeriod(1);
+
+	startTimef = clock();
+	floatloop();
+	endTimef = clock();
+
+	startTimed = clock();
+	doubleloop();
+	endTimed = clock();
+
+	timeEndPeriod(1);
+	printf("float:%ld\ndouble:%ld\n", endTimef - startTimef, endTimed - startTimed);
+	printf("float:%lf[sec.]\ndouble:%lf[sec.]\n", (double)(endTimef - startTimef) / CLOCKS_PER_SEC, (double)(endTimed - startTimed) / CLOCKS_PER_SEC);
+	return 0;
+}
