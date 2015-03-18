ab1b464e09c5c88543ff9d5e59021ef949fec4a8
yumetodo <yume-wikijp@live.jp>
2015-03-18 15:00:58 +0900

    typedef,const,スコープ,auto型,四則演算,型変換,stdint.hについて説明

---
+++ src/increment_decrement.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+int main(void){
+	{
+		int n = 2;
+		/*
+		n = n + 1;
+		const int a = n;
+		*/
+		const int a = ++n;
+		printf("n=%d, a=%d", n, a);
+	}
+	{
+		int n = 2;
+		/*
+		const int a = n;
+		n = n + 1;
+		*/
+		const int a = n++;
+		printf("n=%d, a=%d", n, a);
+	}
+	return 0;
+}
