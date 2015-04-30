3af975004c3c4bf9f07d2b51c56926b75b646602
yumetodo <yume-wikijp@live.jp>
2015-04-30 14:15:48 +0900

    問題の回答を隠す機能を実装。5章のエスケープがおかしいのを修正。

---
+++ scripts/common.js
@@ -0,0 +1,9 @@
+﻿	var change_answer_state = function(id){
+		var d = document.getElementById(id);
+		if(d.style.display == "none"){
+			d.style.display = "block";
+		}
+		else{
+			d.style.display = "none";
+		}
+	}
